087222568976db1c5628cf736adf15ed3c93f7b9
constadry <behappydtworry@gmail.com>
2022-08-26 20:30:21 +0300

    feat: added coping files possibilities and removed over head classes

--- SimpleArchiver/Program.cs
+++ SimpleArchiver/Program.cs
@@ -1,3 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using Newtonsoft.Json;
+using SimpleArchiver.Models;
+
+CopyFiles();
+
+void CopyFiles()
+{
+    var config = GetConfig();
+    var sourceFiles = Directory.GetFiles(config.SourceFolder);
+    foreach (var sourceFile in sourceFiles)
+    {
+        File.Copy(config.SourceFolder + sourceFile, config.TargetFolder + sourceFile);
+    }
+}
+
+Config GetConfig()
+{
+    var jsonConfig = File.ReadAllText("./config.json");
+    var configModel = JsonConvert.DeserializeObject<Config>(jsonConfig);
+
+    return configModel;
+}
